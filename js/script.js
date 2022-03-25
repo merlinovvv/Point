3260c28c23e6fa6cf6b5f859ac2899deb797d0ec
maerlinovvv <merlinovandrej@gmail.com>
2022-03-25 23:04:23 +0200

    new

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 let animItems = document.querySelectorAll('._anim-items');
-
-if(animItems.length > 0){
-    window.addEventListener('scroll', animOnScroll);
-    function animOnScroll(params){
-        for (let index = 0; index < animItems.length; index++) {
-            const animItem = animItems[index];
-            const animItemHeight = animItem.offsetHeight;
-            const animItemOffset = offset(animItem).top;
-            const animStart = 4;
-
-            let animItemPoint = window.innerHeight - animItemHeight / animStart;
-
-            if(animItemHeight > window.innerHeight){
-                animItemPoint = window.innerHeight - window.innerHeight /animStart;
-            }
-
-            if((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)){
-                animItem.classList.add('_active');
-            } else {
-                if(!animItem.classList.contains('_anim-no-hide')){
-                    animItem.classList.remove('_active');
+const screenWidth = window.screen.width;
+    if(animItems.length > 0){
+        window.addEventListener('scroll', animOnScroll);
+        function animOnScroll(params){
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+    
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+    
+                if(animItemHeight > window.innerHeight){
+                    animItemPoint = window.innerHeight - window.innerHeight /animStart;
+                }
+    
+                if((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)){
+                    animItem.classList.add('_active');
+                } else {
+                    if(!animItem.classList.contains('_anim-no-hide')){
+                        animItem.classList.remove('_active');
+                    }
                 }
             }
         }
-    }
-    function offset(el){
-        const rect = el.getBoundingClientRect(),
-            scrollLeft = window.scrollX || document.documentElement.scrollLeft,
-            scrollTop = window.scrollY || document.documentElement.scrollTop;
-        return {
-            top: rect.top + scrollTop,
-            left: rect.left + scrollLeft
+        function offset(el){
+            const rect = el.getBoundingClientRect(),
+                scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+                scrollTop = window.scrollY || document.documentElement.scrollTop;
+            return {
+                top: rect.top + scrollTop,
+                left: rect.left + scrollLeft
+            }
         }
+        setTimeout(() => {
+            animOnScroll(); 
+        }, 300);
     }
-    setTimeout(() => {
-        animOnScroll(); 
-    }, 300);
-}
